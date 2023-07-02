0e571cbb4b35a5bb10838f01b9522da59d2923ad
ivanmiletic <60513195+ivanmiletic@users.noreply.github.com>
2023-07-02 18:57:09 +0000

    Deploying to gh-pages from @ gorules/jdm-editor@58d1843d4b6caa92e3d74251b440e53753505cfa 🚀

---
+++ assets/preview-85b05479.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-e3fc877a.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-6fd11b24.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-6fd11b24.js","./iframe-e3fc877a.js","./index-32a56c20.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-5da88d60.js","./chunk-4NMOSTKD-052c8761.js","./index-d475d2ea.js","./index-d37d4223.js","./extends-b84940d4.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-85b05479.js.map
