6b2c1528d58dae3772667b73d9c4971d78ab6c89
Ivan Miletic <vnmiletic@gmail.com>
2023-07-02 21:43:16 +0200

    fix: lint

--- src/components/decision-table/dialog/dt-dialogs.tsx
+++ src/components/decision-table/dialog/dt-dialogs.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
 
 import { useDecisionTableDialog } from '../context/dt-dialog.context'
 import { ColumnType, TableSchemaItem, useDecisionTable } from '../context/dt.context'
