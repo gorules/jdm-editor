0e12fd3db3c029ff3cd0b9d5e5ef9744c9e39ed3
Ivan Miletic <vnmiletic@gmail.com>
2023-09-05 14:43:13 +0200

    fix: function perf fix

--- src/components/function/function.tsx
+++ src/components/function/function.tsx
@@ -1,9 +1,11 @@
+import { FormatPainterOutlined } from '@ant-design/icons';
 import { Editor, useMonaco } from '@monaco-editor/react';
-import { Spin, theme } from 'antd';
+import { Button, Spin, theme } from 'antd';
 import type { editor } from 'monaco-editor';
-import React, { useEffect, useState } from 'react';
-import { useThrottledCallback } from 'use-debounce';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDebouncedCallback, useThrottledCallback } from 'use-debounce';
 
+import { Stack } from '../stack';
 import { FunctionDebugger } from './function-debugger';
 import './function.scss';
 import { functionDefinitions } from './helpers/libs';
@@ -12,14 +14,31 @@ import type { FunctionDebuggerTrace } from './types';
 export type FunctionProps = {
   disabled?: boolean;
   defaultValue?: string;
+  disableDebug?: boolean;
+  language?: string;
   value?: string;
   onChange?: (value: string) => void;
   trace?: FunctionDebuggerTrace;
 };
 
-export const Function: React.FC<FunctionProps> = ({ disabled = false, defaultValue, value, onChange, trace }) => {
+export const Function: React.FC<FunctionProps> = ({
+  disabled = false,
+  disableDebug,
+  language = 'javascript',
+  defaultValue,
+  value,
+  onChange,
+  trace,
+}) => {
   const monaco = useMonaco();
+  const mountedRef = useRef(false);
   const { token } = theme.useToken();
+  const [innerValue, setInnerValue] = useState<string>();
+
+  const innerChange = useDebouncedCallback((val: string) => {
+    onChange?.(val);
+  }, 50);
+
   const [editor, setEditor] = useState<editor.IStandaloneCodeEditor>();
   const resizeEditor = useThrottledCallback(() => editor?.layout(), 100, { trailing: true });
 
@@ -41,6 +60,17 @@ export const Function: React.FC<FunctionProps> = ({ disabled = false, defaultVal
     monaco.languages.typescript.javascriptDefaults.setExtraLibs(extraLibs);
   }, [monaco]);
 
+  useEffect(() => {
+    if (mountedRef.current && value !== undefined && value !== innerValue) {
+      setInnerValue(value);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    setInnerValue(value === undefined ? defaultValue : value);
+    mountedRef.current = true;
+  }, []);
+
   return (
     <div
       className='grl-function'
@@ -54,25 +84,50 @@ export const Function: React.FC<FunctionProps> = ({ disabled = false, defaultVal
         } as any
       }
     >
-      <Editor
-        loading={<Spin size='large' />}
-        language='javascript'
-        defaultValue={defaultValue}
-        value={value}
-        onMount={(editor) => setEditor(editor)}
-        onChange={(value) => onChange?.(value ?? '')}
-        theme={token.mode === 'dark' ? 'vs-dark' : 'light'}
-        height='100%'
-        options={{
-          automaticLayout: true,
-          contextmenu: false,
-          fontSize: 14,
-          fontFamily: 'var(--mono-font-family)',
-          readOnly: disabled,
-          tabSize: 2,
-        }}
-      />
-      <FunctionDebugger trace={trace} />
+      <Stack
+        horizontal
+        horizontalAlign={'space-between'}
+        verticalAlign={'center'}
+        className={'grl-function__command-bar'}
+      >
+        <Stack gap={8} horizontal className='full-width'>
+          <Button
+            type='default'
+            size={'small'}
+            color='secondary'
+            icon={<FormatPainterOutlined />}
+            disabled={disabled}
+            onClick={() => {
+              editor?.getAction?.('editor.action.formatDocument')?.run?.();
+            }}
+          >
+            Format
+          </Button>
+        </Stack>
+      </Stack>
+      <div className={'grl-function__content'}>
+        <Editor
+          loading={<Spin size='large' />}
+          language={language}
+          value={innerValue}
+          onMount={(editor) => setEditor(editor)}
+          onChange={(value) => {
+            setInnerValue(value ?? '');
+            innerChange(value ?? '');
+          }}
+          theme={token.mode === 'dark' ? 'vs-dark' : 'light'}
+          height='100%'
+          options={{
+            automaticLayout: true,
+            contextmenu: false,
+            fontSize: 14,
+            fontFamily: 'var(--mono-font-family)',
+            readOnly: disabled,
+            tabSize: 2,
+          }}
+        />
+        {!disableDebug && <FunctionDebugger trace={trace} />}
+      </div>
     </div>
   );
 };
