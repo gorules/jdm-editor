fd9e339b3fe1520367d68c906d952617e594b30f
stefan-gorules <127550877+stefan-gorules@users.noreply.github.com>
2023-12-14 09:57:58 +0000

    Deploying to gh-pages from @ gorules/jdm-editor@7eded3ef6ab8f09f76cf27cf58c450c03d030eb9 🚀

---
+++ assets/syntaxhighlighter-4OVWVELQ-495f6a96.js
@@ -0,0 +1,2 @@
+import{S as n,c as d,s as f}from"./DocsRenderer-3PUGWF3O-a53ef7c2.js";import"./iframe-4922d95a.js";import"../sb-preview/runtime.js";import"./index-f15177ee.js";import"./_commonjsHelpers-de833af9.js";import"./_getPrototype-fb556be7.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./extends-0ea0fc03.js";import"./index-356e4a49.js";export{n as SyntaxHighlighter,d as createCopyToClipboardFunction,f as default};
+//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-495f6a96.js.map
---
+++ components/function/monaco.d.ts
@@ -0,0 +1,7 @@
+import type { Monaco } from '@monaco-editor/react';
+declare global {
+    interface Window {
+        monaco?: Monaco;
+    }
+}
+//# sourceMappingURL=monaco.d.ts.map
