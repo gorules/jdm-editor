11f88f1c828c1df58d7e9b85920fc4ca2c88617d
stefan-gorules <127550877+stefan-gorules@users.noreply.github.com>
2024-01-18 17:48:00 +0100

    fix: decision table size (#16)
    * fix: decision table size
    
    * fix styles and table row memoization bug
    
    * bump package

--- src/components/decision-table/table/table-head-row.tsx
+++ src/components/decision-table/table/table-head-row.tsx
@@ -2,20 +2,37 @@ import type { HeaderGroup } from '@tanstack/react-table';
 import { flexRender } from '@tanstack/react-table';
 import clsx from 'clsx';
 import React from 'react';
+import { match } from 'ts-pattern';
 
 export const TableHeadRow: React.FC<{ headerGroup: HeaderGroup<any> }> = ({ headerGroup }) => (
   <tr key={headerGroup.id}>
     <th colSpan={1} style={{ width: 72 }} />
     {headerGroup.headers.map((header) => {
+      const context = header.getContext();
+      const parent = context.header.column.parent?.id;
+
+      const parentKind = match(parent)
+        .with('inputs', () => 'input')
+        .with('outputs', () => 'output')
+        .otherwise(() => undefined);
+
+      const selfKind = match(context.column.id)
+        .with('inputs', () => 'input')
+        .with('outputs', () => 'output')
+        .with('_description', () => 'description')
+        .otherwise(() => undefined);
+
       return (
         <th
           key={header.id}
           colSpan={header.colSpan}
+          data-self={selfKind}
+          data-parent={parentKind}
           style={{
             width: header.getSize(),
           }}
         >
-          {!header.isPlaceholder && flexRender(header.column.columnDef.header, header.getContext())}
+          {!header.isPlaceholder && flexRender(header.column.columnDef.header, context)}
           {header.column.getCanResize() && (
             <div
               className={clsx('resizer', header.column.getIsResizing() && 'isResizing')}
