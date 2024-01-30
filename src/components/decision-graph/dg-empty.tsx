19c91f2b22089ca6cc7b16e8da4b45cdd2c988bc
Stefan <stefan@gorules.io>
2024-01-30 20:41:42 +0100

    fix: component rendering

--- src/components/decision-graph/dg-empty.tsx
+++ src/components/decision-graph/dg-empty.tsx
@@ -64,7 +64,7 @@ export const DecisionGraphEmpty: React.FC<DecisionGraphEmptyType> = ({
       id,
       disabled,
       configurable,
-      components,
+      components: Array.isArray(components) ? components : [],
       simulate,
     });
   }, [id, disabled, configurable, components, simulate]);
