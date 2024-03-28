c6621027900a9f682ebfd1f923f52a8ac1ae11d3
Stefan <stefan@gorules.io>
2024-03-28 17:34:49 +0100

    fix: table jump

--- src/components/decision-table/dt.stories.tsx
+++ src/components/decision-table/dt.stories.tsx
@@ -110,7 +110,7 @@ const stressRules = () => {
   for (let i = 10_000; i > 0; i--) {
     stressRules.push({
       '_id': `${i}`,
-      '_description': `${i} description`,
+      '_description': `${i} description asdainsffi asnofa asniof saion fionas finosafionf saoin`,
       'HVo_JpALi8': `> ${i}`,
       'HW6mSVfLbs': '"US"',
       '3EGDrV0ssV': `${i}`,
--- src/components/decision-table/table/table-row.tsx
+++ src/components/decision-table/table/table-row.tsx
@@ -1,18 +1,20 @@
 import type { Row } from '@tanstack/react-table';
 import { flexRender } from '@tanstack/react-table';
+import type { VirtualItem } from '@tanstack/react-virtual';
 import { Typography } from 'antd';
 import clsx from 'clsx';
-import React, { useMemo, useRef } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 
 import { useDecisionTableActions, useDecisionTableState } from '../context/dt-store.context';
 
 export const TableRow: React.FC<{
-  index: number;
   row: Row<Record<string, string>>;
   reorderRow: (draggedRowIndex: number, targetRowIndex: number) => void;
   disabled?: boolean;
-}> = ({ index, row, reorderRow, disabled }) => {
+  virtualItem: VirtualItem;
+  onResize?: (node: HTMLElement) => void;
+}> = ({ row, reorderRow, disabled, virtualItem, onResize }) => {
   const tableActions = useDecisionTableActions();
   const { cursor, activeRules } = useDecisionTableState(({ cursor, activeRules }) => ({
     cursor,
@@ -42,34 +44,57 @@ export const TableRow: React.FC<{
     return Array.isArray(activeRules) && activeRules.indexOf(row.id) > -1;
   }, [row.id, activeRules]);
 
+  useEffect(() => {
+    if (!trRef.current) {
+      return;
+    }
+
+    onResize?.(trRef.current);
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.target.hasAttribute('data-virtual-index')) {
+          return;
+        }
+
+        onResize?.(entry.target as HTMLElement);
+      });
+    });
+
+    resizeObserver.observe(trRef.current);
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
   return (
     <tr
-      ref={disabled ? undefined : trRef}
+      ref={trRef}
       className={clsx(
         'table-row',
         isDropping && direction === 'down' && 'dropping-down',
         isDropping && direction === 'up' && 'dropping-up',
         isActive && 'active',
         disabled && 'disabled',
-        cursor?.y === index && !disabled && 'selected',
+        cursor?.y === virtualItem.index && !disabled && 'selected',
       )}
       style={{
         opacity: isDragging ? 0.5 : 1,
       }}
+      data-virtual-index={virtualItem.index}
     >
       <td
         className={clsx('sort-handler', !disabled && 'draggable')}
         ref={disabled ? undefined : dragRef}
-        onContextMenuCapture={() => tableActions.setCursor({ x: 'id', y: index })}
+        onContextMenuCapture={() => tableActions.setCursor({ x: 'id', y: virtualItem.index })}
       >
         <div className={'text'}>
-          <Typography>{index + 1}</Typography>
+          <Typography>{virtualItem.index + 1}</Typography>
         </div>
       </td>
       {row.getVisibleCells().map((cell) => (
         <td
           key={cell.id}
-          className={clsx(!disabled && cursor?.x === cell.column.id && cursor?.y === index && 'selected')}
+          className={clsx(!disabled && cursor?.x === cell.column.id && cursor?.y === virtualItem.index && 'selected')}
           style={{ width: cell.column.getSize() }}
         >
           {flexRender(cell.column.columnDef.cell, cell.getContext())}
