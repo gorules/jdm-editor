4b1050f1e96d1717ac8c3768878807a88242c088
Stefan <stefan@gorules.io>
2024-10-24 19:08:06 +0200

    feat: schema validation

--- packages/jdm-editor/src/helpers/omit.ts
+++
@@ -1,9 +0,0 @@
-export function omit<T, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K> {
-  const result = {} as Omit<T, K>;
-  for (const key in obj) {
-    if (!keys.includes(key as unknown as K)) {
-      (result as any)[key] = obj[key];
-    }
-  }
-  return result;
-}
