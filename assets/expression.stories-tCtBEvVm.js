86da58f87cbbfe42c86a3c2eff0cecfda08d667f
stefan-gorules <127550877+stefan-gorules@users.noreply.github.com>
2025-05-23 12:44:43 +0000

    Deploying to gh-pages from @ gorules/jdm-editor@fc6f7fe8140f65b0c0d293aedf664de5463b4d65 🚀

---
+++ assets/expression.stories-tCtBEvVm.js
@@ -0,0 +1,14 @@
+import{j as r}from"./index-CQHSiloJ.js";import{f as p}from"./index-D5UsiwoX.js";import{r as l}from"./index-DQDNmYQF.js";import{E as a}from"./expression-Bxrs_w_9.js";import"./index-DYVtDik4.js";import"./confirm-action-MQ32mxxG.js";import"./AntdIcon-ahp8uH5b.js";import"./wasm-C6WB3NPO.js";import"./ce-preview-CbrvMXO7.js";const o=[{id:"1",key:"customer.fullName",value:'customer.firstName + " " + customer.lastName'},{id:"2",key:"customer.isPremium",value:'contains(customer.tags, "premium")'},{id:"3",key:"customer.purchaseTotals",value:"sum(map(customer.purchases, #.amount))"}],c={customer:{firstName:"John",lastName:"Doe",tags:["premium"],purchases:[{id:"",amount:100}]}},S={title:"Expression",component:a,args:{configurable:!0,disabled:!1,defaultValue:o,inputData:c,onChange:p()},argTypes:{inputData:{control:"object"},manager:{table:{disable:!0}},value:{table:{disable:!0}}}},n=({children:e})=>r.jsx("div",{style:{maxWidth:900},children:e}),s={render:e=>r.jsx(n,{children:r.jsx(a,{...e})})},t={render:e=>{const[u,m]=l.useState(o);return r.jsx(n,{children:r.jsx(a,{value:u,onChange:m,...e})})}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: args => {
+    return <StoryWrapper>
+        <Expression {...args} />
+      </StoryWrapper>;
+  }
+}`,...s.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: args => {
+    const [value, setValue] = useState(expressionDefault);
+    return <StoryWrapper>
+        <Expression value={value} onChange={setValue} {...args} />
+      </StoryWrapper>;
+  }
+}`,...t.parameters?.docs?.source}}};const b=["Uncontrolled","Controlled"];export{t as Controlled,s as Uncontrolled,b as __namedExportsOrder,S as default};
